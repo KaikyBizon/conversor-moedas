aace70f9f710f95c81073b45672a5d5f7640124a
KaikyBizon <kaiky.bizon@gmail.com>
2025-05-22 17:30:23 -0300

    feat: Criação da página principal de conversão com os inputs e botão

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
 import './App.css';
+import Input from './components/input';
 
 function App() {
   return (
-    <>
-      <div>
-        <h1 className="text-3x3 font-bold underline">Hello world!</h1>
+    <section className="w-screen h-screen bg-[#ffeedb] flex items-center font-poppins">
+      <div className="w-screen h-100 flex justify-center items-center">
+        <form className="grid gap-y-4" action="#">
+          <div className="inputs-data flex flex-wrap gap-2 max-w-[550px] justify-between">
+            <div>
+              <Input placeholder="ESCOLHA A MOEDA" />
+              <datalist></datalist>
+            </div>
+            <div>
+              <Input placeholder="ESCOLHA A MOEDA" />
+              <datalist></datalist>
+            </div>
+            <div>
+              <Input placeholder="DIGITE UM VALOR" />
+            </div>
+          </div>
+          <button
+            className="cursor-pointer flex items-center justify-center rounded-lg bg-[#8a2446] max-w-[150px] min-h-[30px] w-full text-white"
+            type="submit"
+          >
+            Converter
+          </button>
+        </form>
       </div>
-    </>
+    </section>
   );
 }
 
---
+++ src/components/input.tsx
@@ -0,0 +1,8 @@
+export default function Input({ ...props }: React.InputHTMLAttributes<HTMLInputElement>) {
+    return(
+        // Input para os dados necessários para conversão
+        <>
+            <input className="rounded-lg pl-[5px] bg-[#7c9eb2] flex-1 min-w-[250px] min-h-[40px] w-full focus:outline-none focus:ring-1 focus:ring-black" name="Input" {...props}/>
+        </>
+    )
+}
